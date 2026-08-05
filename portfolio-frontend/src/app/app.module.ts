37284cf501e7b69905394dea20aac42189456bf2
Mateo Blanco <mateoblanco457@gmail.com>
2026-08-05 11:08:44 -0300

    feat: add about, projects, contact and together sections with shared fixed background

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home/home.component';
+import { AboutComponent } from './features/about/about/about.component';
+import { ProjectsComponent } from './features/projects/projects/projects.component';
+import { ContactComponent } from './features/contact/contact/contact.component';
+import { TogetherComponent } from './features/together/together/together.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    TogetherComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio-frontend/src/app/features/together/together/together.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-together',
+  standalone: false,
+  templateUrl: './together.component.html',
+  styleUrl: './together.component.scss'
+})
+export class TogetherComponent {
+
+}
