b1b38dfcfa0f0af2c8e558701e19a2e5d45d0c1d
Mateo Blanco <mateoblanco457@gmail.com>
2026-08-05 16:20:24 -0300

    refactor: merge cómo pienso into sobre mí and switch proyectos/contacto to dark solid background

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home/home.component';
-import { AboutComponent } from './features/about/about/about.component';
-import { ProjectsComponent } from './features/projects/projects/projects.component';
-import { ContactComponent } from './features/contact/contact/contact.component';
-import { TogetherComponent } from './features/together/together/together.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent,
-    TogetherComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
